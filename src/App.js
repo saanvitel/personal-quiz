eaea3486b91d08bcd47c3e2792e25f55d9cdc9ef
saanvitel <saanvi.telukoti@gmail.com>
2023-05-17 13:48:09 +1000

    tried to apply class to answers, have made it so far to that it is all green, in working state of trying to apply ally solution

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import logo from "./logo.svg";
 import "./App.css";
 
+// contains the data needed to load the questions 
 const data = [
   [ {
     name: "q1",
@@ -110,25 +111,29 @@ const data = [
   }]
 ]
 
+//functional component that forms a single button
 function Button(props) {
   return (
-    <>
+    <div>
     <input type="radio" name={props.name} value={props.value} onClick={props.onClick} />
-    <label for={props.for}>{props.text}</label> <br />
-  </>
+    <label className={props.className} for={props.for}>{props.text}</label> <br />
+  </div>
   );
 }
 
+//loads data and multiple button options to form an actual question
 function Question(props){
   console.log(props.numRight);
-  const classOfText = props.applyResult ? "right" : "wrong"
+  const classOfText = props.applyResult ? "right" : "false"
+  const answer = props.selectedAnswer
 
   return(
-    <>
+    <> 
       {data[props.number].map((el, index) => {
         return <Button className={classOfText} onClick={(event) => {
           props.setNumRight(data[props.number][index].value ? props.numRight + 1 : props.numRight + 0)
           props.handleChange(event);
+          props.setSelectedAnswer((answer) => [...answer, data[props.number][index].text]);
         }} 
         name={data[props.number][index].name} value={data[props.number][index].value} for={data[props.number][index].for} text={data[props.number][index].text} />
       })}
@@ -136,23 +141,26 @@ function Question(props){
   );
 }
 
+
 function Quiz() {
   const [numRight, setNumRight] = useState(0);
   const [showResultText, setShowResultText] = useState(false);
-  const [applyResult, setApplyResult] = useState(false);
-  console.log(numRight); // works but if you reclick on right option it still adds one will fix this later
-
+  const [applyResult, setApplyResult] = useState([]);
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+  console.log( numRight); // works but if you reclick on right option it still adds one will fix this later
+  
   const handleChange = event => {
     console.log("LOAF HANDLECHANGE", event.target.value)
     if (event.target.checked ) {
       console.log("loaf checked", event.target.value)
-      if (event.target.value === true) {
+      if (event.target.value === 'true') {
         setApplyResult(true);
       } else {
         setApplyResult(false);
       }
     } 
   }
+  
 
     return (
     <>
@@ -171,7 +179,7 @@ function Quiz() {
         <p className="question-header">
           Question 1: <br /> What genre of music do I like?
         </p>
-        <Question number={0} setNumRight={setNumRight} numRight={numRight} applyResult={applyResult} handleChange={handleChange}/>
+        <Question number={0} setSelectedAnswer={setSelectedAnswer} selectedAnswer={selectedAnswer} correctAnswer={"Whatever"} setNumRight={setNumRight} numRight={numRight} applyResult={applyResult} handleChange={handleChange}/>
       </div>
 
 
@@ -180,7 +188,7 @@ function Quiz() {
         <p className="question-header">
           Question 2: <br /> What is my favorite cuisine?
         </p>
-        <Question number={1} setNumRight={setNumRight} numRight={numRight} applyResult={applyResult} handleChange={handleChange}/>
+        <Question number={1} setSelectedAnswer={setSelectedAnswer} selectedAnswer={selectedAnswer} correctAnswer={"Indian Food"} setNumRight={setNumRight} numRight={numRight} applyResult={applyResult} handleChange={handleChange}/>
       </div>
 
       {/* <!-- question 3 --> */}
@@ -188,7 +196,7 @@ function Quiz() {
         <p className="question-header">
           Question 3: <br /> What animal am I really scared of?
         </p>
-        <Question number={2} setNumRight={setNumRight} numRight={numRight} applyResult={applyResult} handleChange={handleChange}/>
+        <Question number={2} setSelectedAnswer={setSelectedAnswer} selectedAnswer={selectedAnswer} correctAnswer={"Snails 🐌"} setNumRight={setNumRight} numRight={numRight} applyResult={applyResult} handleChange={handleChange}/>
       </div>
 
       {/* <!-- question 4 -->  */}
@@ -197,7 +205,7 @@ function Quiz() {
           Question 4: <br /> What would I consume for the rest of my life if I
           had to?
         </p>
-        <Question number={3} setNumRight={setNumRight} numRight={numRight} applyResult={applyResult} handleChange={handleChange}/>
+        <Question number={3} setSelectedAnswer={setSelectedAnswer} selectedAnswer={selectedAnswer} correctAnswer={"Potatoes 🥔"} setNumRight={setNumRight} numRight={numRight} applyResult={applyResult} handleChange={handleChange}/>
       </div>
 
       {/* <!-- question 5 --> */}
@@ -205,12 +213,12 @@ function Quiz() {
         <p className="question-header">
           Question 5: <br /> What was my dream occupation as a child?
         </p>
-        <Question number={4} setNumRight={setNumRight} numRight={numRight} applyResult={applyResult} handleChange={handleChange}/>
+        <Question number={4} setSelectedAnswer={setSelectedAnswer} selectedAnswer={selectedAnswer} correctAnswer={"Astronaut"} setNumRight={setNumRight} numRight={numRight} applyResult={applyResult} handleChange={handleChange}/>
       </div>
 
       <div id="button-submit">
         {console.log(showResultText)}
-        <button type="submit" onClick={() => setShowResultText(true)}>
+        <button type="submit" onClick={(e) => {setShowResultText(true); handleChange(e)}}>
           Submit
         </button>
       </div>
