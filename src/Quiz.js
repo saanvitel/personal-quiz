f9a525b5c2b69985cabcf54fd4cf7edab24da227
saanvitel <saanvi.telukoti@gmail.com>
2023-05-18 13:41:20 +1000

    ben helped me with bringing react components a level higher so now it can map over the questions automatically

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,78 +1,106 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
-// contains the data needed to load the questions 
-
-
-const correctAnswer = [
-  "Whatever",
-  "Indian Food",
-  "Snails 🐌",
-  "Potatoes 🥔",
-  "Astronaut"
-]
-
-
 
 //functional component that forms a single button
 function Button(props) {
   return (
     <div>
-    <input type="radio" name={props.name} value={props.value} onClick={props.onClick} />
-    <label className={props.className} for={props.for}>{props.text}</label> <br />
-  </div>
+      <input
+        type="radio"
+        name={props.name}
+        value={props.value}
+        onClick={props.onClick}
+      />
+      <label className={props.className} for={props.for}>
+        {props.text}
+      </label>{" "}
+      <br />
+    </div>
   );
 }
 
 //loads data and multiple button options to form an actual question
-function Question({number, question, showResult, setSelectedAnswer, selectedAnswer, handleChange}){
-  // const classOfText = props.applyResult ? "right" : "false"
-  
-  return(
-    <> 
-      {question.map((answer) => {
+function Answer({
+  number,
+  answers,
+  showResult,
+  setSelectedAnswer,
+  selectedAnswer,
+}) {
+  return (
+    <>
+      {answers.map((answer) => {
         let className = "";
         if (showResult && selectedAnswer == answer.text) {
-          className = answer.value ? "right" : "wrong"
+          className = answer.value ? "right" : "wrong";
         }
-        
-        return <Button className={className} onClick={(event) => {
-          // setNumRight(answer.value ? numRight + 1 : numRight + 0)
-          handleChange(event);
-          setSelectedAnswer((globalAnswerData) => ({ ...globalAnswerData, [number]: answer.text}));
-        }} 
-        name={answer.name} value={answer.value} for={answer.for} text={answer.text} />
+
+        return (
+          <Button
+            className={className}
+            onClick={() => {
+              // setNumRight(answer.value ? numRight + 1 : numRight + 0)
+              setSelectedAnswer((globalAnswerData) => ({
+                ...globalAnswerData,
+                [number]: answer.text,
+              }));
+            }}
+            name={answer.name}
+            value={answer.value}
+            for={answer.for}
+            text={answer.text}
+          />
+        );
       })}
     </>
   );
 }
 
+function Question({ question, showResult, setSelectedAnswer, selectedAnswer }) {
+  const { number, text, answers } = question;
+  return (
+    <>
+      <div className="question-container">
+        <p className="question-header">
+          {text}
+          <Answer
+            number={number}
+            answers={answers}
+            showResult={showResult}
+            setSelectedAnswer={setSelectedAnswer}
+            selectedAnswer={selectedAnswer}
+          />
+        </p>
+      </div>
+    </>
+  );
+}
 
-function Quiz({questions}) {
-  // const [numRight, setNumRight] = useState(0);
+function Quiz({ questions }) {
   const [showResultText, setShowResultText] = useState(false);
   const [showResult, setShowResult] = useState(false);
   const [selectedAnswer, setSelectedAnswer] = useState({});
   let numsRight = 0;
-  console.log(numsRight); // works but if you reclick on right option it still adds one will fix this later
-  console.log(selectedAnswer);
 
-  const handleChange = event => {
-    
-  }
-  
+  const correctAnswer = [
+    "Whatever",
+    "Indian Food",
+    "Snails 🐌",
+    "Potatoes 🥔",
+    "Astronaut",
+  ];
+
   function findNumRight() {
-    for (let i=0 ; i < correctAnswer.length; i++ ) {
+    for (let i = 0; i < correctAnswer.length; i++) {
       if (correctAnswer[i] === selectedAnswer[i]) {
-        numsRight += 1
+        numsRight += 1;
       }
     }
-    console.log(selectedAnswer);
-    return numsRight
+    return numsRight;
   }
 
-    return (
+  return (
     <>
       <div id="header">
         <h1>Quiz About Myself</h1>
@@ -84,63 +112,37 @@ function Quiz({questions}) {
         </p>
       </div>
 
-      {/* <!-- question 1 --> */}
-      <div className="question-container">
-        <p className="question-header">
-          Question 1: <br /> What genre of music do I like?
-        </p>
-        <Question number={0} question={questions[0]} showResult={showResult} setSelectedAnswer={setSelectedAnswer} selectedAnswer={selectedAnswer[0]} correctAnswer={"Whatever"}  numsRight={numsRight} handleChange={handleChange}/>
-      </div>
-
-
-      {/* <!-- question 2 -->  */}
-      <div className="question-container">
-        <p className="question-header">
-          Question 2: <br /> What is my favorite cuisine?
-        </p>
-        <Question number={1} question={questions[1]} showResult={showResult} setSelectedAnswer={setSelectedAnswer} selectedAnswer={selectedAnswer[1]} correctAnswer={"Indian Food"}  numsRight={numsRight} handleChange={handleChange}/>
-      </div>
-
-      {/* <!-- question 3 --> */}
-      <div className="question-container">
-        <p className="question-header">
-          Question 3: <br /> What animal am I really scared of?
-        </p>
-        <Question number={2} question={questions[2]} showResult={showResult} setSelectedAnswer={setSelectedAnswer} selectedAnswer={selectedAnswer[2]} correctAnswer={"Snails 🐌"}  numsRight={numsRight} handleChange={handleChange}/>
-      </div>
-
-      {/* <!-- question 4 -->  */}
-      <div className="question-container">
-        <p className="question-header">
-          Question 4: <br /> What would I consume for the rest of my life if I
-          had to?
-        </p>
-        <Question number={3} question={questions[3]} showResult={showResult} setSelectedAnswer={setSelectedAnswer} selectedAnswer={selectedAnswer[3]} correctAnswer={"Potatoes 🥔"}  numsRight={numsRight} handleChange={handleChange}/>
-      </div>
-
-      {/* <!-- question 5 --> */}
-      <div className="question-container">
-        <p className="question-header">
-          Question 5: <br /> What was my dream occupation as a child?
-        </p>
-        <Question number={4} question={questions[4]} showResult={showResult} setSelectedAnswer={setSelectedAnswer} selectedAnswer={selectedAnswer[4]} correctAnswer={"Astronaut"}  numsRight={numsRight} handleChange={handleChange}/>
-      </div>
+      {/* renders questions  */}
+      {questions.map((question, index) => {
+        return (
+          <Question
+            question={question}
+            showResult={showResult}
+            setSelectedAnswer={setSelectedAnswer}
+            selectedAnswer={selectedAnswer[index]}
+          />
+        );
+      })}
 
       <div id="button-submit">
-        {console.log(showResultText)}
-        <button type="submit" onClick={(e) => {setShowResultText(true); handleChange(e); setShowResult(true); }}>
+        <button
+          type="submit"
+          onClick={(e) => {
+            setShowResultText(true);
+            setShowResult(true);
+          }}
+        >
           Submit
         </button>
       </div>
 
-      <p id="footer">{showResultText ? `You got ${findNumRight()} correct!` : "Thank you for playing this quiz!"}</p>
-      {console.log(showResultText)}
+      <p id="footer">
+        {showResultText
+          ? `You got ${findNumRight()} correct!`
+          : "Thank you for playing this quiz!"}
+      </p>
     </>
   );
 }
 
 export default Quiz;
-
-
-
-
--- src/data.js
+++ src/data.js
@@ -1,107 +1,292 @@
 export const data = [
-    [ {
+  {
+    number: 0,
+    text: `Question 1:
+        What genre of music do I like?`,
+    answers: [
+      {
+        name: "q1",
+        value: false,
+        for: "Kpop",
+        text: "Kpop",
+      },
+      {
+        name: "q1",
+        value: false,
+        for: "Indie",
+        text: "Indie",
+      },
+      {
+        name: "q1",
+        value: false,
+        for: "Indian",
+        text: "Indian",
+      },
+      {
+        name: "q1",
+        value: true,
+        for: "Whatever",
+        text: "Whatever",
+      },
+    ],
+  },
+  {
+    number: 1,
+    text: "Question 2: What is my favorite cuisine?",
+    answers: [
+      {
+        name: "q2",
+        value: false,
+        for: "italian",
+        text: "Italian Food",
+      },
+      {
+        name: "q2",
+        value: false,
+        for: "mexican",
+        text: "Mexican Food",
+      },
+      {
+        name: "q2",
+        value: true,
+        for: "indian",
+        text: "Indian Food",
+      },
+      {
+        name: "q2",
+        value: false,
+        for: "thai",
+        text: "Thai Food",
+      },
+    ],
+  },
+  {
+    number: 2,
+    text: "Question 3: <br /> What animal am I really scared of?",
+    answers: [
+      {
+        name: "q3",
+        value: false,
+        for: "crows",
+        text: "Crows 🦅",
+      },
+      {
+        name: "q3",
+        value: false,
+        for: "snake",
+        text: "Snakes 🐍",
+      },
+      {
+        name: "q3",
+        value: true,
+        for: "snails",
+        text: "Snails 🐌",
+      },
+      {
+        name: "q3",
+        value: false,
+        for: "spiders",
+        text: "Spiders 🕷️",
+      },
+    ],
+  },
+  {
+    number: 3,
+    text: "Question 4: <br /> What would I consume for the rest of my life if I had to?",
+    answers: [
+      {
+        name: "q4",
+        value: false,
+        for: "bubbletea",
+        text: "Bubble Tea 🧋",
+      },
+      {
+        name: "q4",
+        value: false,
+        for: "lettuce",
+        text: "Lettuce 🥬",
+      },
+      {
+        name: "q4",
+        value: false,
+        for: "curry",
+        text: "Curry 🥘 ",
+      },
+      {
+        name: "q4",
+        value: true,
+        for: "potatoes",
+        text: "Potatoes 🥔 ",
+      },
+    ],
+  },
+  {
+    number: 4,
+    text: "Question 5: <br /> What was my dream occupation as a child?",
+    answers: [
+      {
+        name: "q5",
+        value: false,
+        for: "gamesdesigner",
+        text: "Computer games designer",
+      },
+      {
+        name: "q5",
+        value: false,
+        for: "interiordes",
+        text: "Interior Designer",
+      },
+      {
+        name: "q5",
+        value: true,
+        for: "astronaut",
+        text: "Astronaut",
+      },
+      {
+        name: "q5",
+        value: false,
+        for: "doctor",
+        text: "Doctor",
+      },
+    ],
+  },
+];
+
+export const olddata = [
+  [
+    `Question 1:
+     What genre of music do I like?`,
+    {
       name: "q1",
       value: false,
       for: "Kpop",
-      text:"Kpop"
-    }, {
+      text: "Kpop",
+    },
+    {
       name: "q1",
       value: false,
       for: "Indie",
-      text:"Indie"
-    }, {
+      text: "Indie",
+    },
+    {
       name: "q1",
       value: false,
       for: "Indian",
-      text: "Indian"
-    } , {
+      text: "Indian",
+    },
+    {
       name: "q1",
       value: true,
       for: "Whatever",
-      text: "Whatever"
-    } ],
-    [{
+      text: "Whatever",
+    },
+  ],
+  [
+    "Question 2: <br /> What is my favorite cuisine?",
+    {
       name: "q2",
       value: false,
       for: "italian",
-      text:"Italian Food"
-    },{
+      text: "Italian Food",
+    },
+    {
       name: "q2",
       value: false,
       for: "mexican",
-      text:"Mexican Food"
-    },{
+      text: "Mexican Food",
+    },
+    {
       name: "q2",
       value: true,
       for: "indian",
-      text:"Indian Food"
-    },{
+      text: "Indian Food",
+    },
+    {
       name: "q2",
       value: false,
       for: "thai",
-      text:"Thai Food"
-    }],
-    [{
+      text: "Thai Food",
+    },
+  ],
+  [
+    "Question 3: <br /> What animal am I really scared of?",
+    {
       name: "q3",
       value: false,
       for: "crows",
-      text:"Crows 🦅"
-    },{
+      text: "Crows 🦅",
+    },
+    {
       name: "q3",
       value: false,
       for: "snake",
-      text:"Snakes 🐍"
-    }, {
+      text: "Snakes 🐍",
+    },
+    {
       name: "q3",
       value: true,
       for: "snails",
-      text:"Snails 🐌"
-    }, {
+      text: "Snails 🐌",
+    },
+    {
       name: "q3",
       value: false,
       for: "spiders",
-      text:"Spiders 🕷️"
-    }],
-    [{
+      text: "Spiders 🕷️",
+    },
+  ],
+  [
+    "Question 4: <br /> What would I consume for the rest of my life if I had to?",
+    {
       name: "q4",
       value: false,
       for: "bubbletea",
-      text:"Bubble Tea 🧋"
-    },{
+      text: "Bubble Tea 🧋",
+    },
+    {
       name: "q4",
       value: false,
       for: "lettuce",
-      text:"Lettuce 🥬"
-    },{
+      text: "Lettuce 🥬",
+    },
+    {
       name: "q4",
       value: false,
       for: "curry",
-      text:"Curry 🥘 "
-    },{
+      text: "Curry 🥘 ",
+    },
+    {
       name: "q4",
       value: true,
       for: "potatoes",
-      text:"Potatoes 🥔 "
-    }],
-    [{
+      text: "Potatoes 🥔 ",
+    },
+  ],
+  [
+    "Question 5: <br /> What was my dream occupation as a child?",
+    {
       name: "q5",
       value: false,
       for: "gamesdesigner",
-      text:"Computer games designer"
-    },{
+      text: "Computer games designer",
+    },
+    {
       name: "q5",
       value: false,
       for: "interiordes",
-      text:"Interior Designer"
-    },{
+      text: "Interior Designer",
+    },
+    {
       name: "q5",
       value: true,
       for: "astronaut",
-      text:"Astronaut"
-    },{
+      text: "Astronaut",
+    },
+    {
       name: "q5",
       value: false,
       for: "doctor",
-      text:"Doctor"
-    }]
-  ]
+      text: "Doctor",
+    },
+  ],
+];
